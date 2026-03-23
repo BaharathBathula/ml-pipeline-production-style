f1e7d8e25a8e5abc53168abcccdcddda12997086
Baharath Bathula <143721016+BaharathBathula@users.noreply.github.com>
2026-03-23 11:43:07 -0500

    Add test for home endpoint in test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,1 +1,8 @@
+from fastapi.testclient import TestClient
+from src.api.app import app
 
+client = TestClient(app)
+
+def test_home():
+    response = client.get("/")
+    assert response.status_code == 200
