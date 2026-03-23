240aa4e0b275436b7bd12bd3a508b7f82fd7dcfd
Baharath Bathula <143721016+BaharathBathula@users.noreply.github.com>
2026-03-23 11:51:38 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,9 @@
+from setuptools import find_packages, setup
 
+setup(
+    name="ml_pipeline_production_style",
+    version="0.1.0",
+    author="Baharath Bathula",
+    packages=find_packages(),
+    install_requires=[],
+)
