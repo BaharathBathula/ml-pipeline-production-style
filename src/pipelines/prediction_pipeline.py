e8fbbe21c84559e6d24629cf7b03d249ecaec11c
Baharath Bathula <143721016+BaharathBathula@users.noreply.github.com>
2026-03-23 11:59:49 -0500

    Update prediction_pipeline.py

--- src/pipelines/prediction_pipeline.py
+++ src/pipelines/prediction_pipeline.py
@@ -1,1 +1,12 @@
+import pandas as pd
+from src.utils.common import load_object
 
+
+class PredictionPipeline:
+    def __init__(self, model_path="artifacts/models/model.pkl"):
+        self.pipeline = load_object(model_path)
+
+    def predict(self, features: dict):
+        input_df = pd.DataFrame([features])
+        prediction = self.pipeline.predict(input_df)[0]
+        return prediction
