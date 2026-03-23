db0ba7c1ac0ba23aca5b465d785507b92e8d899c
Baharath Bathula <143721016+BaharathBathula@users.noreply.github.com>
2026-03-23 11:58:31 -0500

    Update model_evaluation.py

--- src/components/model_evaluation.py
+++ src/components/model_evaluation.py
@@ -1,1 +1,23 @@
+import pandas as pd
+from sklearn.metrics import classification_report
 
+from src.utils.logger import logger
+
+
+class ModelEvaluation:
+    def __init__(self, pipeline, target_column):
+        self.pipeline = pipeline
+        self.target_column = target_column
+
+    def evaluate(self, test_path):
+        logger.info("Starting model evaluation")
+
+        test_df = pd.read_csv(test_path)
+        X_test = test_df.drop(columns=[self.target_column])
+        y_test = test_df[self.target_column].map({"Yes": 1, "No": 0}).fillna(test_df[self.target_column])
+
+        predictions = self.pipeline.predict(X_test)
+        report = classification_report(y_test, predictions, output_dict=True)
+
+        logger.info("Model evaluation completed")
+        return report
