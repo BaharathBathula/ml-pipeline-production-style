bc04b013b83b5be68ae9a30394370f51eb5ddd56
Baharath Bathula <143721016+BaharathBathula@users.noreply.github.com>
2026-03-23 11:56:35 -0500

    Update artifact_entity.py

--- src/entity/artifact_entity.py
+++ src/entity/artifact_entity.py
@@ -1,1 +1,13 @@
+from dataclasses import dataclass
 
+
+@dataclass
+class DataIngestionArtifact:
+    train_file_path: str
+    test_file_path: str
+
+
+@dataclass
+class ModelTrainerArtifact:
+    model_path: str
+    metrics_path: str
