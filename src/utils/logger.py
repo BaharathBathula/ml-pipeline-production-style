0cd55320142a1b67f22d7bb04627163720f6972d
Baharath Bathula <143721016+BaharathBathula@users.noreply.github.com>
2026-03-23 11:54:44 -0500

    Update logger.py

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -5,10 +5,10 @@ from datetime import datetime
 LOG_DIR = "logs"
 os.makedirs(LOG_DIR, exist_ok=True)
 
-log_file = os.path.join(LOG_DIR, f"{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.log")
+LOG_FILE = os.path.join(LOG_DIR, f"{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.log")
 
 logging.basicConfig(
-    filename=log_file,
+    filename=LOG_FILE,
     format="[ %(asctime)s ] %(levelname)s - %(name)s - %(message)s",
     level=logging.INFO,
 )
