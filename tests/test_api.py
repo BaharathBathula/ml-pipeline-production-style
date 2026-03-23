db5d5e19fd708e280c44ab701a659f09c846c363
Baharath Bathula <143721016+BaharathBathula@users.noreply.github.com>
2026-03-23 12:01:03 -0500

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,6 +3,13 @@ from src.api.app import app
 
 client = TestClient(app)
 
+
 def test_home():
     response = client.get("/")
     assert response.status_code == 200
+
+
+def test_health():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json()["status"] == "ok"
