b52719ae4dba6a1331050c0d9ea7a91a6ecd53c4
Baharath Bathula <143721016+BaharathBathula@users.noreply.github.com>
2026-03-23 11:56:15 -0500

    Update config_entity.py

--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -1,1 +1,15 @@
+from dataclasses import dataclass
 
+
+@dataclass
+class TrainingPipelineConfig:
+    project_name: str
+    raw_data_path: str
+    train_path: str
+    test_path: str
+    target_column: str
+    test_size: float
+    random_state: int
+    model_path: str
+    metrics_path: str
+    experiment_name: str
